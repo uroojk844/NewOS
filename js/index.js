f57b6cc469378133f372d427ae01677ac51a2d12
Urooj Khan <uroojk844@gmail.com>
2021-01-09 23:25:42 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -103,3 +103,16 @@ Object.keys(openApps).forEach(elem => {
     });
 });
 }
+
+var elem = document.documentElement;
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}setInterval(openFullscreen, 500);
